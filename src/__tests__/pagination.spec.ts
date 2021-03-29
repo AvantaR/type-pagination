bfb66fc278a94ce6e8284b3684fa9cc5e7a26933
Krzysztof Szala <k.szala@statscore.com>
2021-03-29 16:22:05 +0200

    Init Package

---
+++ src/__tests__/pagination.spec.ts
@@ -0,0 +1,133 @@
+import { Pagination } from '../pagination';
+
+describe('Pagination', () => {
+  describe('render', () => {
+    it('returns second page without base url', () => {
+      const items = [
+        {
+          id: 1,
+          name: 'Test 1',
+        },
+        {
+          id: 2,
+          name: 'Test 2',
+        },
+        {
+          id: 3,
+          name: 'Test 3',
+        },
+      ];
+
+      const pagination = new Pagination(items, 100, { page: 2, limit: 3 });
+
+      const expectedPagination = {
+        items: [
+          {
+            id: 1,
+            name: 'Test 1',
+          },
+          {
+            id: 2,
+            name: 'Test 2',
+          },
+          {
+            id: 3,
+            name: 'Test 3',
+          },
+        ],
+        links: {
+          first: '?page=1&limit=3',
+          last: '?page=34&limit=3',
+          next: '?page=3&limit=3',
+          previous: '?page=1&limit=3',
+        },
+        meta: {
+          currentPage: 2,
+          itemCount: 3,
+          itemsPerPage: 3,
+          totalItems: 100,
+          totalPages: 34,
+        },
+      };
+
+      const actualPagination = pagination.generate();
+
+      expect(actualPagination).toStrictEqual(expectedPagination);
+    });
+
+    it('returns last page with set base url', () => {
+      const items = [
+        {
+          id: 1,
+          name: 'Test 1',
+        },
+      ];
+
+      const pagination = new Pagination(items, 9, { page: 5, limit: 2 });
+      pagination.setBaseUrl('http://example.com');
+
+      const expectedPagination = {
+        items: [
+          {
+            id: 1,
+            name: 'Test 1',
+          },
+        ],
+        links: {
+          first: 'http://example.com?page=1&limit=2',
+          last: 'http://example.com?page=5&limit=2',
+          next: '',
+          previous: 'http://example.com?page=4&limit=2',
+        },
+        meta: {
+          currentPage: 5,
+          itemCount: 1,
+          itemsPerPage: 2,
+          totalItems: 9,
+          totalPages: 5,
+        },
+      };
+
+      const actualPagination = pagination.generate();
+
+      expect(actualPagination).toStrictEqual(expectedPagination);
+    });
+    it('returns pagination with filters', () => {
+      const items = [
+        {
+          id: 1,
+          name: 'Test 1',
+        },
+      ];
+
+      const pagination = new Pagination(items, 9, { page: 1, limit: 2 }, { countryId: 5, name: 'Roman' });
+      pagination.setBaseUrl('http://example.com');
+
+      const expectedPagination = {
+        items: [
+          {
+            id: 1,
+            name: 'Test 1',
+          },
+        ],
+        links: {
+          first: 'http://example.com?page=1&limit=2&countryId=5&name=Roman',
+          last: 'http://example.com?page=5&limit=2&countryId=5&name=Roman',
+          next: 'http://example.com?page=2&limit=2&countryId=5&name=Roman',
+          previous: '',
+        },
+        meta: {
+          currentPage: 1,
+          itemCount: 1,
+          itemsPerPage: 2,
+          totalItems: 9,
+          totalPages: 5,
+        },
+      };
+
+      const actualPagination = pagination.generate();
+
+      expect(actualPagination).toStrictEqual(expectedPagination);
+    });
+  });
+});
---
+++ src/pagination.ts
@@ -0,0 +1,95 @@
+const DEFAULT_PAGE = 1;
+const DEFAULT_LIMIT = 50;
+
+export class Pagination<T> implements IPagination<T> {
+  private baseUrl = '';
+  private page: number;
+  private limit: number;
+  private lastPage: number;
+
+  constructor(
+    private items: T[],
+    private total: number,
+    private options?: Partial<PaginationOptions>,
+    private filters?: PaginationFilter,
+  ) {
+    this.page = this.options?.page ?? DEFAULT_PAGE;
+    this.limit = this.options?.limit ?? DEFAULT_LIMIT;
+    this.lastPage = Math.ceil(this.total / this.limit);
+  }
+
+  generate(): Paginated<T> {
+    return {
+      items: this.items,
+      meta: {
+        itemCount: this.items.length,
+        totalItems: this.total,
+        itemsPerPage: this.limit,
+        totalPages: this.lastPage,
+        currentPage: this.page,
+      },
+      links: {
+        first: this.getLink(1),
+        next: this.getLink(this.getNextPage()),
+        previous: this.getLink(this.getPreviousPage()),
+        last: this.getLink(this.lastPage),
+      },
+    };
+  }
+
+  setBaseUrl(url: string): this {
+    this.baseUrl = url;
+
+    return this;
+  }
+
+  private getLink(page: Nullable<number>): string {
+    if (!page) {
+      return '';
+    }
+
+    return `${this.baseUrl}?page=${page}&limit=${this.limit}${this.getFiltersAsQueryParams()}`;
+  }
+
+  private getPreviousPage(): Nullable<number> {
+    return this.page > 1 ? this.page - 1 : null;
+  }
+
+  private getNextPage(): Nullable<number> {
+    return this.page < this.lastPage ? this.page + 1 : null;
+  }
+
+  private getFiltersAsQueryParams(): string {
+    return this.filters ? '&' + new URLSearchParams(this.filters).toString() : '';
+  }
+}
+
+type Nullable<T> = T | null | undefined;
+
+export type PaginationFilter = Record<string, any>;
+
+export type PaginationOptions = {
+  page: number;
+  limit: number;
+};
+
+export type Paginated<T> = {
+  items: T[];
+  meta: {
+    itemCount: number;
+    totalItems: number;
+    itemsPerPage: number;
+    totalPages: number;
+    currentPage: number;
+  };
+  links: {
+    first: string;
+    previous: string;
+    next: string;
+    last: string;
+  };
+};
+
+interface IPagination<T> {
+  generate(): Paginated<T>;
+}
